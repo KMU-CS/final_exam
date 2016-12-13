b6365d88d2d6250cec7c74b9a8823b05418d00e9
허윤서 <sally159357@naver.com>
2016-12-13 22:03:25 +0900

    g

--- 20153242/src/androidTest/java/com/example/a20153242/ExampleInstrumentedTest.java
+++ 20153242/src/androidTest/java/com/example/a20153242/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.igx.problem1;
+package com.example.a20153242;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.igx.problem1", appContext.getPackageName());
+        assertEquals("com.example.a20153242", appContext.getPackageName());
     }
 }
